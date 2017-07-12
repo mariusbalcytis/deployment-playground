3e1393983990f247f178aec72eccb220df303c58
Marius Balčytis <marius.balcytis@gmail.com>
2017-07-13 00:44:33 +0300

    Use-case with cache

---
+++ deployer/app/cache.php
@@ -0,0 +1,18 @@
+<?php
+
+$key = 'key';
+
+$result = apcu_fetch($key);
+if (!$result) {
+    // let's assume we parse some XML/YAML file or make some action here
+    $result = __DIR__;
+
+    apcu_store($key, $result);
+}
+
+header('Content-Type: application/json');
+
+echo json_encode([
+    '__DIR__' => __DIR__,
+    'cached __DIR__' => $result,
+], JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
